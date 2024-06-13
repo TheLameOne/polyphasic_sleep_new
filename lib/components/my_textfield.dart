a6c014e474d4b0d40e7e6a19fc1f0db1a781fe1a
Harsh Verma <vermaharsh463@gmail.com>
2024-06-14 02:11:15 +0530

    implementation of automatic scrolling in textfield

--- lib/components/my_textfield.dart
+++ lib/components/my_textfield.dart
@@ -3,11 +3,13 @@ import 'package:flutter/material.dart';
 class MyTextfield extends StatelessWidget {
   final String hintText;
   final TextEditingController controller;
+  final FocusNode? focusNode;
 
   const MyTextfield({
     super.key,
     required this.hintText,
     required this.controller,
+    this.focusNode,
   });
 
   @override
@@ -18,6 +20,7 @@ class MyTextfield extends StatelessWidget {
       child: SizedBox(
         width: size.width - 80,
         child: TextField(
+          focusNode: focusNode,
           controller: controller,
           decoration: InputDecoration(
               enabledBorder: OutlineInputBorder(
--- lib/pages/how_it_works_page.dart
+++ lib/pages/how_it_works_page.dart
@@ -18,6 +18,37 @@ class HowItWorksPage extends StatefulWidget {
 
 class _HowItWorksPageState extends State<HowItWorksPage> {
   final TextEditingController _messageController = TextEditingController();
+  final ScrollController _scrollController = ScrollController();
+
+  FocusNode myFocusNode = FocusNode();
+
+  @override
+  void initState() {
+    super.initState();
+    myFocusNode.addListener(() {
+      if (myFocusNode.hasFocus) {
+        Future.delayed(
+          const Duration(milliseconds: 200),
+          () => scrollDown(),
+        );
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    myFocusNode.dispose();
+    _messageController.dispose();
+    super.dispose();
+  }
+
+  void scrollDown() {
+    _scrollController.animateTo(
+      _scrollController.position.maxScrollExtent,
+      duration: Duration(milliseconds: 500),
+      curve: Curves.fastOutSlowIn,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,84 +57,93 @@ class _HowItWorksPageState extends State<HowItWorksPage> {
         appBar: AppBar(
           title: Text("How it Works"),
         ),
-        bottomNavigationBar: _buildUserInput(),
-        body: SingleChildScrollView(
-          child: FutureBuilder(
-              future: readJsonData(),
-              builder: (context, data) {
-                if (data.hasError) {
-                  return Center(child: Text("${data.error}"));
-                } else if (data.hasData) {
-                  var qna = data.data as List<QnaModel>;
-                  return Column(
-                    children: [
-                      for (int i = 0; i < qna.length; i++)
-                        Column(
+        body: Column(
+          // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Expanded(
+              child: SingleChildScrollView(
+                controller: _scrollController,
+                child: FutureBuilder(
+                    future: readJsonData(),
+                    builder: (context, data) {
+                      if (data.hasError) {
+                        return Center(child: Text("${data.error}"));
+                      } else if (data.hasData) {
+                        var qna = data.data as List<QnaModel>;
+                        return Column(
                           children: [
-                            Padding(
-                              padding:
-                                  const EdgeInsets.symmetric(vertical: 8.0),
-                              child: BubbleSpecialOne(
-                                text: qna[i].question!,
-                                color: Color(0xFF1B97F3),
-                                tail: true,
-                                isSender: true,
-                                textStyle: TextStyle(
-                                    color: Colors.white, fontSize: 16),
-                              ),
-                            ),
-                            for (int j = 0; j < qna[i].answer!.length; j++)
-                              (j == 0)
-                                  ? BubbleSpecialOne(
-                                      text: qna[i].answer![j],
-                                      color: Color(0xFFE8E8EE),
+                            for (int i = 0; i < qna.length; i++)
+                              Column(
+                                children: [
+                                  Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        vertical: 8.0),
+                                    child: BubbleSpecialOne(
+                                      text: qna[i].question!,
+                                      color: Color(0xFF1B97F3),
                                       tail: true,
-                                      isSender: false,
-                                    )
-                                  : BubbleSpecialOne(
-                                      text: qna[i].answer![j],
-                                      color: Color(0xFFE8E8EE),
-                                      tail: false,
-                                      isSender: false,
+                                      isSender: true,
+                                      textStyle: TextStyle(
+                                          color: Colors.white, fontSize: 16),
                                     ),
+                                  ),
+                                  for (int j = 0;
+                                      j < qna[i].answer!.length;
+                                      j++)
+                                    (j == 0)
+                                        ? BubbleSpecialOne(
+                                            text: qna[i].answer![j],
+                                            color: Color(0xFFE8E8EE),
+                                            tail: true,
+                                            isSender: false,
+                                          )
+                                        : BubbleSpecialOne(
+                                            text: qna[i].answer![j],
+                                            color: Color(0xFFE8E8EE),
+                                            tail: false,
+                                            isSender: false,
+                                          ),
+                                ],
+                              ),
                           ],
-                        ),
-                    ],
-                  );
-                } else {
-                  return Center(
-                    child: CircularProgressIndicator(),
-                  );
-                }
-              }),
+                        );
+                      } else {
+                        return Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      }
+                    }),
+              ),
+            ),
+            _buildUserInput()
+          ],
         ));
   }
 
   Widget _buildUserInput() {
-    return Expanded(
-      child: Container(
-        margin: EdgeInsets.all(8),
-        child: SizedBox(
-          child: Row(
-            children: [
-              MyTextfield(
-                hintText: "Ask me more about Polyphasic Sleep",
-                controller: _messageController,
-              ),
-              Container(
-                decoration: BoxDecoration(
-                    color: Color.fromARGB(220, 76, 175, 79),
-                    shape: BoxShape.circle),
-                child: IconButton(
-                    onPressed: () {},
-                    icon: Icon(
-                      Icons.arrow_upward,
-                      color: Theme.of(context).colorScheme.surface,
-                    )),
-              )
-            ],
+    return Padding(
+      padding: EdgeInsets.symmetric(vertical: 8),
+      child: Row(
+        children: [
+          Expanded(
+            child: MyTextfield(
+              focusNode: myFocusNode,
+              hintText: "Ask me more about Polyphasic Sleep",
+              controller: _messageController,
+            ),
           ),
-        ),
+          Container(
+            decoration: BoxDecoration(
+                color: Color.fromARGB(220, 76, 175, 79),
+                shape: BoxShape.circle),
+            child: IconButton(
+                onPressed: () {},
+                icon: Icon(
+                  Icons.arrow_upward,
+                  color: Theme.of(context).colorScheme.surface,
+                )),
+          )
+        ],
       ),
     );
   }
