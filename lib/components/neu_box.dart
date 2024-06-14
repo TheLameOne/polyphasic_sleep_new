846fa86ba0671e72c060df02ab4e4b159559232f
Harsh Verma <vermaharsh463@gmail.com>
2024-06-14 11:00:39 +0530

    neu box component

---
+++ lib/components/neu_box.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class NeuBox extends StatelessWidget {
+  final Widget? child;
+  const NeuBox({
+    super.key,
+    required this.child,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+          color: Theme.of(context).colorScheme.surface,
+          boxShadow: [
+            BoxShadow(
+                color: Colors.grey.shade500,
+                blurRadius: 15,
+                offset: const Offset(4, 4)),
+            BoxShadow(
+                color: Colors.white,
+                blurRadius: 15,
+                offset: const Offset(-4, -4)),
+          ]),
+    );
+  }
+}
--- lib/pages/how_it_works_page.dart
+++ lib/pages/how_it_works_page.dart
@@ -28,7 +28,7 @@ class _HowItWorksPageState extends State<HowItWorksPage> {
     myFocusNode.addListener(() {
       if (myFocusNode.hasFocus) {
         Future.delayed(
-          const Duration(milliseconds: 200),
+          const Duration(milliseconds: 500),
           () => scrollDown(),
         );
       }
@@ -63,56 +63,59 @@ class _HowItWorksPageState extends State<HowItWorksPage> {
             Expanded(
               child: SingleChildScrollView(
                 controller: _scrollController,
-                child: FutureBuilder(
-                    future: readJsonData(),
-                    builder: (context, data) {
-                      if (data.hasError) {
-                        return Center(child: Text("${data.error}"));
-                      } else if (data.hasData) {
-                        var qna = data.data as List<QnaModel>;
-                        return Column(
-                          children: [
-                            for (int i = 0; i < qna.length; i++)
-                              Column(
-                                children: [
-                                  Padding(
-                                    padding: const EdgeInsets.symmetric(
-                                        vertical: 8.0),
-                                    child: BubbleSpecialOne(
-                                      text: qna[i].question!,
-                                      color: Color(0xFF1B97F3),
-                                      tail: true,
-                                      isSender: true,
-                                      textStyle: TextStyle(
-                                          color: Colors.white, fontSize: 16),
+                child: Padding(
+                  padding: const EdgeInsets.only(bottom: 8.0),
+                  child: FutureBuilder(
+                      future: readJsonData(),
+                      builder: (context, data) {
+                        if (data.hasError) {
+                          return Center(child: Text("${data.error}"));
+                        } else if (data.hasData) {
+                          var qna = data.data as List<QnaModel>;
+                          return Column(
+                            children: [
+                              for (int i = 0; i < qna.length; i++)
+                                Column(
+                                  children: [
+                                    Padding(
+                                      padding: const EdgeInsets.symmetric(
+                                          vertical: 8.0),
+                                      child: BubbleSpecialOne(
+                                        text: qna[i].question!,
+                                        color: Color(0xFF1B97F3),
+                                        tail: true,
+                                        isSender: true,
+                                        textStyle: TextStyle(
+                                            color: Colors.white, fontSize: 16),
+                                      ),
                                     ),
-                                  ),
-                                  for (int j = 0;
-                                      j < qna[i].answer!.length;
-                                      j++)
-                                    (j == 0)
-                                        ? BubbleSpecialOne(
-                                            text: qna[i].answer![j],
-                                            color: Color(0xFFE8E8EE),
-                                            tail: true,
-                                            isSender: false,
-                                          )
-                                        : BubbleSpecialOne(
-                                            text: qna[i].answer![j],
-                                            color: Color(0xFFE8E8EE),
-                                            tail: false,
-                                            isSender: false,
-                                          ),
-                                ],
-                              ),
-                          ],
-                        );
-                      } else {
-                        return Center(
-                          child: CircularProgressIndicator(),
-                        );
-                      }
-                    }),
+                                    for (int j = 0;
+                                        j < qna[i].answer!.length;
+                                        j++)
+                                      (j == 0)
+                                          ? BubbleSpecialOne(
+                                              text: qna[i].answer![j],
+                                              color: Color(0xFFE8E8EE),
+                                              tail: true,
+                                              isSender: false,
+                                            )
+                                          : BubbleSpecialOne(
+                                              text: qna[i].answer![j],
+                                              color: Color(0xFFE8E8EE),
+                                              tail: false,
+                                              isSender: false,
+                                            ),
+                                  ],
+                                ),
+                            ],
+                          );
+                        } else {
+                          return Center(
+                            child: CircularProgressIndicator(),
+                          );
+                        }
+                      }),
+                ),
               ),
             ),
             _buildUserInput()
